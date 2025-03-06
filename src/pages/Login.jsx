64da29b7329b3761a3073bc771e7f539952888bd
A_Chacin <alechacin08@gmail.com>
2025-03-06 09:16:54 -0600

    Actualización de Sign y Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,15 +2,16 @@ import {useForm} from 'react-hook-form';;
 import { useNavigate } from 'react-router-dom';
 import { Card, Button, Form } from 'react-bootstrap';
 import { NavLink, useNavigate } from 'react-router-dom';
-import userService from '@/services/userService';
+import loginUserService from '@/services/userService';
 
 const Login = () => {
 
     const {register, handleSubmit, formState: {errors}} = useForm();
     const navigate = useNavigate(); //Redirigir a otra ruta.
-    const onSubmit = (data) => {
+    const onSubmit = async (data) => {
+        
         try {
-            const response = loginUserService (data);
+            const response = await loginUserService (data);
             if (response === 200) {
                 navigate('/');
                 console.log('Usuario atenticado', response);
@@ -26,31 +27,33 @@ const Login = () => {
             <Card.Body>
                 <Card.Title>Login</Card.Title>
                 <Form onSubmit={handleSubmit(onSubmit)}>
-                    <Form.Group className="mb-3 mt-5" controlId="formBasicEmail">
+                    <Form.Group className='mb-3 mt-5' controlId='formBasicEmail'>
                         <Form.Label>Email:</Form.Label>
                         <Form.Control 
-                            type="email" 
-                            placeholder="Enter email" 
+                            type='email' 
+                            name='email'
+                            placeholder='Enter email' 
                             {...register('email')}
                             required 
                         />
                         <p>{errors.email?.message}</p>
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Group className='mb-3' controlId='formBasicPassword'>
                         <Form.Label>Password</Form.Label>
                         <Form.Control 
-                            type="password" 
-                            placeholder="Password" 
+                            type='password' 
+                            name='password'
+                            placeholder='Password' 
                             {...register('password')}
                             required 
                         />
                         <p>{errors.password?.message}</p>
                     </Form.Group>
                     
-                    <Button variant="success" type="submit" className="me-2" required>Login</Button>
-                    <Button variant="success" type="submit" className="me-2">
-                        <NavLink to="/Sign">Register</NavLink>
+                    <Button variant='success' type='submit' className='me-2' required>Login</Button>
+                    <Button variant='success' type='submit' className='me-2'>
+                        <NavLink to='/Sign'>Register</NavLink>
                     </Button>
                 </Form>
             </Card.Body>
--- src/pages/Sign.jsx
+++ src/pages/Sign.jsx
@@ -1,34 +1,73 @@
 import { Card, Button, Form } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-
+import registerUserService from '@/services/userService';
 const Sign = () => {
+
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const navigate = useNavigate();
+    const onSubmit = async (data) => {
+            try {
+                const response = await registerUserService (data);
+                if (response === 201) {
+                    navigate('/login');
+                    console.log('Usuario creado exitosamente', response);
+                }
+            } catch (error) {
+                console.log('Ocurrio un error en la creacion de usuario', error);
+            }
+        }
     return (
         <Card style={{ width: '22rem' }} className='justify-content-center mx-auto mt-5' > 
             
-            <Card.Body className="text-center">
+            <Card.Body className='text-center'>
                 <Card.Title>Sign Up</Card.Title>
-                <Form>
-                    <Form.Group className="mb-3 mt-5" controlId="formBasicEmail">
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    <Form.Group className='mb-3 mt-5' controlId='formBasicEmail'>
                         <Form.Label>First Name:</Form.Label>
-                        <Form.Control type="text" placeholder="First name" required />
+                        <Form.Control 
+                            type='text' 
+                            name= 'firstName'
+                            placeholder='First name' 
+                            required 
+                        />
+                        {...register('firstName')}
                     </Form.Group>
                     
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Group className='mb-3' controlId='formBasicEmail'>
                         <Form.Label>Last Name:</Form.Label>
-                        <Form.Control type="text" placeholder="Last name" required />
+                        <Form.Control 
+                            type='text' 
+                            name= 'lastName'
+                            placeholder='Last name' 
+                            required 
+                        />
+                        {...register ('lastName')}
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Group className='mb-3' controlId='formBasicEmail'>
                         <Form.Label>Email:</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" required />
+                        <Form.Control  
+                            type='email' 
+                            name = 'email'
+                            placeholder='Enter email' 
+                            required 
+                        />
+                        {...register('email')}
+                        <p>{errors.email?.message}</p>
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Group className='mb-3' controlId='formBasicPassword'>
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" required />
+                        <Form.Control  
+                            type='password' 
+                            name='password'
+                            placeholder='Password' 
+                            required />
+                        {...register('password')}
+                        <p>{errors.password?.message}</p>
                     </Form.Group>
                     
-                    <Button variant="success" type="submit" className="me-2">
+                    <Button variant='success' type='submit' className='me-2'>
                         <NavLink to={'/Login'}>Login</NavLink>
                     </Button>
                 </Form>
