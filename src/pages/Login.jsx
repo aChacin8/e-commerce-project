83160a058dd9caafdcc485901bba5ee246f0856f
A_Chacin <alechacin08@gmail.com>
2025-03-07 09:44:56 -0600

    Modificacion de Login y Sign Up

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import {useForm} from 'react-hook-form';;
-import { useNavigate } from 'react-router-dom';
 import { Card, Button, Form } from 'react-bootstrap';
 import { NavLink, useNavigate } from 'react-router-dom';
 import loginUserService from '@/services/userService';
--- src/pages/Sign.jsx
+++ src/pages/Sign.jsx
@@ -1,6 +1,8 @@
 import { Card, Button, Form } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import registerUserService from '@/services/userService';
+import { useNavigate } from 'react-router-dom';
+
 const Sign = () => {
 
     const {register, handleSubmit, formState: {errors}} = useForm();
@@ -22,26 +24,46 @@ const Sign = () => {
             <Card.Body className='text-center'>
                 <Card.Title>Sign Up</Card.Title>
                 <Form onSubmit={handleSubmit(onSubmit)}>
-                    <Form.Group className='mb-3 mt-5' controlId='formBasicEmail'>
+                    <Form.Group className='mb-3 mt-5' controlId='formBasicFirstName'>
                         <Form.Label>First Name:</Form.Label>
                         <Form.Control 
                             type='text' 
                             name= 'firstName'
                             placeholder='First name' 
-                            required 
+                            required
+                            {...register('firstName')} 
                         />
-                        {...register('firstName')}
+                        <p>{errors.firstName?.message}</p>
+
                     </Form.Group>
                     
-                    <Form.Group className='mb-3' controlId='formBasicEmail'>
+                    <Form.Group className='mb-3' controlId='formBasicLastName'>
                         <Form.Label>Last Name:</Form.Label>
                         <Form.Control 
                             type='text' 
                             name= 'lastName'
                             placeholder='Last name' 
-                            required 
+                            required
+                            {...register ('lastName')} 
                         />
-                        {...register ('lastName')}
+                        <p>{errors.lastName?.message}</p>
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='formBasicGender'>
+                        <Form.Label>Gender:</Form.Label>
+                        <Form.Select 
+                            className='mb-3' 
+                            controlId='formBasicGender' 
+                            name='gender'
+                            onChange={()=>{}}
+                            {...register ('gender')}
+                            required
+                        >
+                            <option value = 'M'>Male</option>
+                            <option value = 'F'>Female</option>
+                            <option value = 'O'>other</option>
+                        </Form.Select>
+                        <p>{errors.gender?.message}</p>
                     </Form.Group>
 
                     <Form.Group className='mb-3' controlId='formBasicEmail'>
@@ -50,9 +72,9 @@ const Sign = () => {
                             type='email' 
                             name = 'email'
                             placeholder='Enter email' 
-                            required 
+                            required
+                            {...register('email')} 
                         />
-                        {...register('email')}
                         <p>{errors.email?.message}</p>
                     </Form.Group>
 
@@ -62,8 +84,9 @@ const Sign = () => {
                             type='password' 
                             name='password'
                             placeholder='Password' 
-                            required />
-                        {...register('password')}
+                            required 
+                            {...register('password')}
+                            />
                         <p>{errors.password?.message}</p>
                     </Form.Group>
                     
