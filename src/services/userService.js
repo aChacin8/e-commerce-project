eb5286f06e291cf366024c608bf7a45511b74056
A_Chacin <alechacin08@gmail.com>
2025-03-07 13:22:42 -0600

    se corrigio la llamada a la API

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const URL = 'https://json-sever-jwt.onrender.com/';
+const URL = 'https://json-sever-jwt.onrender.com';
 
 //signup
-const registerUserService = async (data) => axios.post (`${URL}/register`, data) //https://json-sever-jwt.onrender.com/register {json}
+const registerUserService =  (data) => axios.post (`${URL}/register`, data) //https://json-sever-jwt.onrender.com/register {json}
 
 //login
-const loginUserService = async (data) => axios.post (`${URL}/login`, data)  // https://json-sever-jwt.onrender.com/login {json}
+const loginUserService =  (data) => axios.post (`${URL}/login`, data)  // https://json-sever-jwt.onrender.com/login {json}
 
 export default  {registerUserService, loginUserService};
