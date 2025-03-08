9949eb8eb9cc7a70a361214ce46b7154672ae20f
A_Chacin <alechacin08@gmail.com>
2025-03-08 17:46:10 -0600

    Creacion de Secret (Tipo usuario)

--- src/pages/Secret.jsx
+++ src/pages/Secret.jsx
@@ -1,8 +1,32 @@
+import {useAuthContext} from '@/hooks/useAuthContext';
+import { Card } from 'react-bootstrap';
+
 const Secret = () => {
+    const {userPayload} = useAuthContext();
+    
     return (
-        <div>
-            <h1>Secret</h1>
-        </div>
+        <>
+            <h1>Tipo de Usuario:</h1>
+            {
+                userPayload?.role === 'ADMIN'
+                    ? <Card>
+                        <Card.Body>
+                            <Card.Title>ADMIN</Card.Title>
+                            <Card.Text>
+                                Bienvenido Administrador
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    : <Card>
+                        <Card.Body>
+                            <Card.Title>USER</Card.Title>
+                            <Card.Text>
+                                Bienvenido Usuario
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+            }
+        </>
     )
 }
 
