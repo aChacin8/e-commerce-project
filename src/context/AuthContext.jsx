ac91f6a27d0b6f8922bbdf2a654c07ee6a142ca7
A_Chacin <alechacin08@gmail.com>
2025-03-08 13:42:00 -0600

    Se creo el contexto

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useState, useEffect } from "react";
+import {jwtDecode} from 'jwt-decode';
+
+const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+    const [isAuth, setIsAuth] = useState(false);  //Estoy autenticado
+    const [userPayload, setUserPayload] = useState(null); //JWT payload decodificando
+    
+    const login = (token) => {
+        localStorage.setItem ('token', token);
+        const decode = jwtDecode(token);
+        setUserPayload (decode);
+        setIsAuth(true);
+    }
+
+    const logout = () => {
+        localStorage.removeItem ('token');
+        setUserPayload (null); //se borra el Payload
+        setIsAuth(false); //se cierra sesion
+    }
+    
+    useEffect(()=> {
+        const token = localStorage.getItem('token');
+        if(token){
+            const decode = jwtDecode(token);
+            setUserPayload(decode);
+            setIsAuth(true);
+        }
+    },[])
+
+    const data = {
+        isAuth,
+        userPayload,
+        login,
+        logout
+    };
+    
+    return(
+        <>
+            <AuthContext.Provider value={data}>
+                {children}
+            </AuthContext.Provider>
+        </>
+    );
+}
+export default AuthProvider;
