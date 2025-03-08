52fe0f85af63828b6b0858950bcb4f62126de2ad
A_Chacin <alechacin08@gmail.com>
2025-03-08 16:03:55 -0600

    Proteccion de rutas

--- src/routes/RoutesIndex.jsx
+++ src/routes/RoutesIndex.jsx
@@ -1,13 +1,28 @@
 import {Routes, Route} from 'react-router-dom';
 import {Home, Dashboard, Secret, Login, Sign} from '@/pages';
+import { useAuthContext } from '@/hooks/useAuthContext';
 
 
 const RoutesIndex = () => {
+    const {isAuth} = useAuthContext();
+
     return ( 
         <Routes>
             <Route path='/' element= {<Home/>}/>
-            <Route path='/Dashboard' element= {<Dashboard/>}/>
-            <Route path='/Secret' element= {<Secret/>}/>
+            <Route path='/Dashboard' 
+            element= {
+            isAuth
+                ? <Dashboard/>
+                : <Login/>
+            }
+            />
+            <Route path='/Secret' 
+            element={ 
+            isAuth 
+                ? <Secret/> 
+                : <Login/>
+            }
+            />
             <Route path='/Login' element= {<Login/>}/>  
             <Route path='/Sign' element= {<Sign/>}/>
         </Routes>
