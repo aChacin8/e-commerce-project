dbc3f398290cafa24d2290490978c07ec10197ba
A_Chacin <alechacin08@gmail.com>
2025-03-08 13:49:07 -0600

    Se modificaron archivos anteriores

--- src/hooks/useProductContext.jsx
+++
@@ -1,9 +0,0 @@
-import {ProductContextComponent} from "@/context/ProductContextComponent";
-import { useContext } from "react";
-
-export const useProductContext = () =>{
-    return useContext (ProductContextComponent);
-
-
-}
-
