3365adaf8e18201bf93d413a7ff3be24beb9003d
A_Chacin <alechacin08@gmail.com>
2025-03-08 17:46:38 -0600

    Contenido de la Pagina (productos)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,47 @@
-
+import { useState, useEffect } from "react";
+import  {getAllItemService} from '@/services/itemService';
+import { Card } from "react-bootstrap";
 
 const Home = () => {
+    const [itemsList, setItemsList] = useState([]);
+
+    useEffect(() => {
+        const fecthItemsData = async () => {
+            try {
+                const response = await getAllItemService();
+
+                if (response.status === 200) {
+                    setItemsList(response.data);
+                }
+
+            } catch (error) {
+                console.log('Ocurrio un error', error);
+            }
+        }
+        fecthItemsData();
+    },[]);
+
+    
     return (
-        <div>
+        <>
             <h1>Home</h1>
-        </div>
+            <div className='d-flex justify-content-center flex-wrap'>
+                {itemsList.map((item) => (
+                    <Card key={item.id} style={{ width: '18rem' }} className='m-2'>
+                        <Card.Img variant="top" src={item.image} />
+                        <Card.Body>
+                            <Card.Title>{item.name}</Card.Title>
+                            <Card.Text>
+                                {item.description}
+                            </Card.Text>
+                            <Card.Text>
+                                Price: {item.price}$
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                ))}
+            </div>
+        </>
     )
 }
 
