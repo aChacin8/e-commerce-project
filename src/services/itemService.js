4840459d0894b5f043badd0bd85528ba9b04a7e8
A_Chacin <alechacin08@gmail.com>
2025-03-08 16:25:15 -0600

    Consumir la data de productos de la API

---
+++ src/services/itemService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { get } from 'react-hook-form';
+import { data } from 'react-router-dom';
+
+const URL = 'https://json-sever-jwt.onrender.com';
+
+const getAllItemsService = () => axios.get (`${URL}/items`, data);
+
+const getOneItemService = (id) => axios.get (`${URL}/items/${id}`,data);
+
+const createItemService = (data, token) => axios.post (`${URL}/items`, data, {headers: {Authorzation:`Bearer ${token}`}});
+
+
+export default  {getAllItemsService, getOneItemService, createItemService};
