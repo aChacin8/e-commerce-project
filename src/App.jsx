c6d415cbb0573f91a5efcfb2f50dccd19dee713f
A_Chacin <alechacin08@gmail.com>
2025-03-16 23:24:10 -0600

    arreglos en las referencias

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import RoutesIndex from '@/routes/RoutesIndex';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from '@/context/AuthContext';
 import { useState } from 'react';
+import { ShoppingCartProvider } from './context/ShoppingCartContext';
 
 function App() {
   const [searchItem, setSearchItem] = useState('');
@@ -11,10 +12,12 @@ function App() {
   return (
     <>
       <AuthProvider>
-        <BrowserRouter>
+      <ShoppingCartProvider>
+      <BrowserRouter>
           <Header setSearchItem={setSearchItem}/>
           <RoutesIndex searchItem={searchItem}/>
         </BrowserRouter>
+      </ShoppingCartProvider>
       </AuthProvider>
     </>
   )
