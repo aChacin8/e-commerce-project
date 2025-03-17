74c48d11f9973afc0906ce68624c60de977ebced
A_Chacin <alechacin08@gmail.com>
2025-03-16 23:25:37 -0600

    Agregamos funciones al botón "add to cart"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,51 +1,82 @@
 import { useState, useEffect } from "react";
-import  {getAllItemService} from '@/services/itemService';
+import { getAllItemService } from "@/services/itemService";
 import { Button, Card } from "react-bootstrap";
-import '@/styles/Home.scss';
-const Home = ({searchItem}) => {
+import { useShoppingCartContext } from "@/hooks/useShoppingCartContext";
+import {useAuthContext} from '@/hooks/useAuthContext';
+import { NavLink } from "react-router-dom";
+
+import "@/styles/Home.scss";
+
+
+const Home = ({ searchItem }) => {
     const [itemsList, setItemsList] = useState([]);
+    const { cart, setCart } = useShoppingCartContext();
+    const {isAuth} = useAuthContext();
 
     useEffect(() => {
-        const fecthItemsData = async () => {
+        const fetchItemsData = async () => {
             try {
                 const response = await getAllItemService();
-
                 if (response.status === 200) {
                     setItemsList(response.data);
                 }
             } catch (error) {
-                console.log('Ocurrio un error', error);
+                console.log("Ocurrió un error", error);
             }
-        }
-        fecthItemsData();
-    },[]);
+        };
+        fetchItemsData();
+    }, []);
 
-    const filteredItems = itemsList.filter(item=> {
-        return item.product_name.toLowerCase().includes(searchItem.toLowerCase());
-    });
+    const filteredItems = itemsList.filter((item) =>
+        item.product_name.toLowerCase().includes(searchItem.toLowerCase())
+    );
+
+    const addToCart = (item) => {
+        setCart((inicialCart) => {
+            const itemExists = inicialCart.find((cartItem) => cartItem.id === item.id);
+            
+            if (itemExists) {
+                return inicialCart.map((cartItem) =>
+                    cartItem.id === item.id
+                        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                        : cartItem
+                );
+            } else {
+                return [...inicialCart, { ...item, quantity: 1 }];
+            }
+        });
+    };
+
+    const alert = () =>{
+        return (
+            window.alert ('You should Log in first')
+        )
+    }
 
     return (
         <>
             <h1 className="home__title">Home</h1>
-            <div className='home__products'>
+            <div className="home__products">
                 {filteredItems.map((item) => (
-                    <Card key={item.id} className='home__card'>
-                        <Card.Img variant="top" src={item.image} className="home__card-img" />
+                    <Card key={item.id} className="home__card">
+                        <Card.Img variant="top" src={item.image}alt={item.product_name} className="home__card-img" />
                         <Card.Body className="home__card-body">
                             <Card.Title className="home__card-title">{item.product_name}</Card.Title>
-                            <Card.Text className="home__card-text">
-                                {item.description}
-                            </Card.Text>
-                            <Card.Text className="home__card-price">
-                                Price: {item.price}$
-                            </Card.Text>
-                            <Button variant="success" className="home__card-button">Add to Cart</Button>
+                            <Card.Text className="home__card-text">{item.description}</Card.Text>
+                            <Card.Text className="home__card-price">Price: {item.price}$</Card.Text>
+                            <Button 
+                                variant="success" 
+                                className="home__card-button"
+                                onClick={() => {isAuth ? addToCart(item) : alert()}}
+                            >
+                                Add to Cart
+                            </Button>
                         </Card.Body>
                     </Card>
                 ))}
             </div>
         </>
-    )
-}
+    );
+};
 
 export default Home;
