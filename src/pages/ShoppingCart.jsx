1cbda87a51d72398456f09c6139bc27765876577
A_Chacin <alechacin08@gmail.com>
2025-03-18 23:27:42 -0600

    Creacion de botones de compra

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -24,18 +24,19 @@ const ShoppingCart = () => {
 
     return (
         <>
-            <div className='shop__products'>
+            <div className='shop__products' >
 
                 {cart.length === 0 ? (
                     <p>Tu carrito está vacío</p>
-                    ) : (
-                        cart.map((item) => (
-                            <Card key={item.id} className="shop__card" style={{ width: '18rem' }}>
-                                <Card.Body>
-                                    <Card.Img src={item.image} alt={item.product_name} style={{ width: '10rem' }} />
-                                    <Card.Title className="shop__card-name">{item.product_name}</Card.Title>
-                                    <Card.Text className="shop__card-price">Precio: ${item.price}</Card.Text>
-                                    <Card.Text className="shop__card-quantity">Cantidad: {item.quantity}</Card.Text>
+                ) : (
+                    cart.map((item) => (
+                        <Card key={item.id} className="shop__card" style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <Card.Img src={item.image} alt={item.product_name} style={{ width: '10rem' }} />
+                                <Card.Title className="shop__card-name">{item.product_name}</Card.Title>
+                                <Card.Text className="shop__card-price">Precio: ${item.price}</Card.Text>
+                                <Card.Text className="shop__card-quantity">Cantidad: {item.quantity}</Card.Text>
+                                <div>
                                     <Button
                                         variant="danger"
                                         onClick={() => decreaseQuantity(item.id)}
@@ -44,19 +45,36 @@ const ShoppingCart = () => {
                                         -
                                     </Button>
                                     <Button
-                                        variant="success"
+                                        variant="primary"
                                         onClick={() => increaseQuantity(item.id)}
                                         className="shopping__card-button"
                                     >
                                         +
                                     </Button>
-                                </Card.Body>
-                            </Card>
-                        
+                                </div>
+                                <Button
+                                    className='mt-3'
+                                    variant='success'
+                                    onClick={() => a}
+                                >
+                                    Comprar
+                                </Button>
+                            </Card.Body>
+                        </Card>
+
                     ))
-                    
+
                 )}
             </div>
+            {cart.length === 0 ? (
+                <br />
+            ) : (
+                <div className='button'>
+                    <button className='button__buy' >Comprar Carrito</button>
+                    <button className='button__clear'>Vaciar Carrito</button>
+                </div>
+            )}
+
         </>
     );
 }
